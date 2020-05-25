da4d52b77f1eb2d184f0d56ab5eb067ce5011828
Horus Camacho <horuscamachoavila@MacBook-Pro-de-Horus.local>
2020-05-25 14:43:05 -0500

    NavBar ready to be programed

---
+++ kunhi/src/container/AppNav/AppNav.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const AppNav = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand col-1" href="#">KÜNHI</a>
+            <div classNameName="container row d-flex justify-content-end col-11">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </nav>
+    )
+}
+
+export default AppNav
---
+++ kunhi/src/container/Principal/Principal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Principal = () => {
+    return (
+        <div className="container">
+        Hola mundo probando la app
+    </div>
+    )
+}
+
+export default Principal
